7b58760309031db088a47411919117f459e3da22
0HW75 <huangxiwen0423@gmail.com>
2026-01-29 19:04:43 +0800

    Add frontend App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import NavigationPage from './pages/NavigationPage'
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationPage />
+    </div>
+  )
+}
+
+export default App
